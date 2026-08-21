Testing Fixed Points Numerically

code/AI-Skill-FP-1-Testing-Nondim.py
@@ -41,35 +41,29 @@ from scipy.integrate import solve_ivp
 
 
 # amount of work needed to improve skill
-ws = 2
+ws = 0.5
 
 
-# time scale for w
-a = 0.01
+
 # time scale for s
-b = 0.01
+b = 1
 # time scale for P 
-g = 0.01
-
-
-
-#sai = 1.3*smax
+g = 1
 
 
 
 # productivity skill threshold
-#sp = smax/3
+
 sp = 1/2
-#sp = 1.2*smax
+
 
 
 
 # Motivation Array
-Ms = np.array([-0.25,0.25])
-sais = np.array([1/4,3/4])
-
-
-M =1
+#Ms = np.array([-0.25,0, 0.25])
+#sais = np.array([sp/2,sp,3*sp/2])
+Ms = np.array([-0.25, 0.25])
+sais = np.array([sp/2,3*sp/2])
 
 
 
@@ -93,9 +87,9 @@ def fun(t,y):
 
 
 
-tspan = [0,10000]
+tspan = [0,1000]
 
-fig, axs = plt.subplots(len(Ms), len(sais))
+fig, axs = plt.subplots(len(Ms), len(sais), layout="constrained")
 for i in range(len(Ms)):
     M =Ms[i]
     for j in range(len(sais)):
@@ -104,9 +98,9 @@ for i in range(len(Ms)):
         [w,s,P] = sol.y
         t1 = sol.t
         axs[i, j].plot(t1,w,t1,s,'-.',t1,P,'--')
-        if i == 0:
+        if i < len(Ms)-1:
             axs[i, j].set_xticklabels([])
-        axs[i, j].set_title(f"M = {M}, $s_{{ai}}$ = {sai}")
+        axs[i, j].set_title(f"M = {M}, $s_{{ai}}$ = {sai}, $s_{{P}}$ = {sp}", fontsize=12)
         if j == len(sais)-1:
             plt.legend(["$w_D$","$s_{ID}$","$P$"])
 
@@ -128,5 +122,9 @@ plt.show()
 
 
 #plt.show()
+
+fig.savefig("FP1_Nondim_Stability.pdf")
+fig.savefig("FP1_Nondim_Stability.eps",format='eps')
+fig.savefig("FP1_Nondim_Stability.png")
 
 

code/AI-Skill-FP-10-Testing-Nondim.py
@@ -81,11 +81,12 @@ tspan = [0,1000]
 
 paramshape= np.shape(param1)
 
-fig, axs = plt.subplots(len(wss), paramshape[0])
+fig, axs = plt.subplots(len(wss), paramshape[0], layout="constrained")
 for i in range(paramshape[0]):
     sai,sp= param1[i]
     for j in range(len(wss)):
         ws =wss[j]
+        print((sp-(1-ws)*sai)-ws )
         w0 = (sai-sp)/(sai-1)
         if w0 > 1 or w0 < 0:
             print("Impossible Initial Condition")
@@ -100,38 +101,17 @@ for i in range(paramshape[0]):
         axs[i, j].plot(t1,w,t1,s,'-.',t1,P,'--')
         if i == 0:
             axs[i, j].set_xticklabels([])
-        axs[i, j].set_title(f"$s_{{ai}}$ = {sai}, $s_{{P}}$ = {sp}, $w_{{s}}$ = {ws}")
+        axs[i, j].set_title(f"$s_{{ai}}$ = {sai}, $s_{{P}}$ = {sp}, $w_{{s}}$ = {ws}", fontsize=12)
+        if j == len(wss)-1:
+            plt.legend(["$w_D$","$s_{ID}$","$P$"])       
 
 
 
 
 
-#fig = plt.figure(figsize=(12, 10))
-#subplots = [fig.add_subplot(2, 2, i + 1, projection='3d') for i in range(4)]
 
 
 
-#for i, ax in enumerate(subplots):
-#    i2 = np.mod(i,2)
-#    j2 = np.mod(i+1,2)
-#    ws =wss[i2]
-#    sai= sais[j2] 
-#    
-#    w0 = wd*(sai-sp)/(sai-smax)
-#    if w0 > wd or w0 < 0:
-#        print("Impossible Initial Condition")
-#        print(w0)
-#    s0 = smax
-#    P0 = M
-#    y0 = [w0, s0, P0]
-#    y0 = y0 +100*np.array([np.random.uniform(0,0.01), -np.random.uniform(0,0.01),np.random.uniform(0,0.01)])
-#    sol = solve_ivp(fun, tspan, y0, method = 'RK45',atol=1e-10,rtol=1e-8) 
-#    [w,s,P] = sol.y
-#    t1 = sol.t
-#    ax.plot(w,s,P,label='Phase Curve')
-#    ax.legend()
-
-
-#plt.tight_layout()
-#plt.show()
-
+fig.savefig("FP10_Nondim_Stability.pdf")
+fig.savefig("FP10_Nondim_Stability.eps",format='eps')
+fig.savefig("FP10_Nondim_Stability.png")

code/AI-Skill-FP-3-Testing-Nondim.py
@@ -8,19 +8,19 @@ This is code testing the numerical thresholds
 
 For stability of fixed points.
 
-This tests FP 2 stability
+This tests FP 1 stability
 
 Need to test cases:
     
-1. M <Pmax, sai < sp (stable)
-2. M <Pmax, sai > sp (unstable)
-3. M <Pmax, sai = sp (unstable)
-4. M =Pmax, sai < sp (unstable)
-5. M =Pmax, sai > sp (unstable)
-6. M =Pmax, sai = sp (unstable)
-7. M >Pmax, sai < sp (unstable)
-8. M >Pmax, sai > sp (unstable)
-9. M >Pmax, sai = sp (unstsable)
+1. M < 0, sai < sp (stable)
+2. M <0, sai > sp (unstable)
+3. M <0, sai = sp (unstable)
+4. M = 0, sai < sp (unstable)
+5. M = 0, sai > sp (unstable)
+6. M = 0, sai = sp (unstable)
+7. M >0, sai < sp (unstable)
+8. M >0, sai > sp (unstable)
+9. M >0, sai = sp (unstsable)
 
 
 """
@@ -36,44 +36,39 @@ from scipy.integrate import solve_ivp
 
 # Parameters
 
-
-
-
-
 # amount of work needed to improve skill
 ws = 0.5
 
 
 
 # time scale for s
-b = 1
+b =1
 # time scale for P 
 g = 1
 
 
 
-
 # productivity skill threshold
-
 sp = 1/2
 
 
 
 
 # Motivation Array
-#Ms = np.array([0.75,1,1.25])
-#sais = np.array([sp/2,sp,3*sp/2])
-Ms = np.array([0.75,1.25])
-sais = np.array([sp/2,3*sp/2])
+Ms = np.array([-0.25,0, 0.25])
+sais = np.array([sp/2,sp,3*sp/2])
+
+
 
 
 
 # FP 1
 w0 = 0
-s0 = 0
-P0 = 1
-y0 = [w0, s0, P0] 
-y0 = y0+np.array([np.random.uniform(0,0.1), np.random.uniform(0,0.1),-np.random.uniform(0,0.1)])
+s0 = 1
+P0 = 0
+y0 = [w0, s0, P0]
+# Perturbation
+y0 = y0+np.array([np.random.uniform(0,0.1), -np.random.uniform(0,0.1),np.random.uniform(0,0.1)])
 
 # function for ODE
 def fun(t,y):
@@ -81,6 +76,7 @@ def fun(t,y):
   dsdt = b*(y[0]-ws)*(1-y[1])*y[1] # equation for ds/dt
   dPdt = g*((y[1]-sp)*y[0]+ ((sai-sp))*(1-y[0]))*(1-y[2])*y[2] # equation for dP/dt
   return [dwdt, dsdt, dPdt] # return array for dw/dt ds/dt
+
 
 
 tspan = [0,1000]
@@ -96,13 +92,10 @@ for i in range(len(Ms)):
         axs[i, j].plot(t1,w,t1,s,'-.',t1,P,'--')
         if i < len(Ms)-1:
             axs[i, j].set_xticklabels([])
-        axs[i, j].set_title(f"M = {M}, $s_{{ai}}$ = {sai}, $s_{{P}}$ = {sp}", fontsize=12)
+        axs[i, j].set_title(f"M = {M}, $s_{{ai}}$ = {sai}, $s_{{P}}$ = {sp}", fontsize=9)
         if j == len(sais)-1:
             plt.legend(["$w_D$","$s_{ID}$","$P$"])
 
-
-
-#plt.ylabel("Function Values")
 plt.show()
 
 #fig2 =plt.figure()
@@ -120,9 +113,8 @@ plt.show()
 
 #plt.show()
 
+fig.savefig("FP3_Nondim_Stability.pdf")
+fig.savefig("FP3_Nondim_Stability.eps",format='eps')
+fig.savefig("FP3_Nondim_Stability.png")
 
 
-
-fig.savefig("FP2_Nondim_Stability.pdf")
-fig.savefig("FP2_Nondim_Stability.eps",format='eps')
-fig.savefig("FP2_Nondim_Stability.png")

code/AI-Skill-FP-4-Testing-Nondim.py
@@ -44,9 +44,8 @@ from scipy.integrate import solve_ivp
 ws = 0.5
 
 
-
 # time scale for s
-b = 1
+b =1
 # time scale for P 
 g = 1
 
@@ -61,19 +60,17 @@ sp = 1/2
 
 
 # Motivation Array
-#Ms = np.array([0.75,1,1.25])
-#sais = np.array([sp/2,sp,3*sp/2])
-Ms = np.array([0.75,1.25])
-sais = np.array([sp/2,3*sp/2])
+Ms = np.array([0.75,1,1.25])
+sais = np.array([sp/2,sp,3*sp/2])
 
 
 
 # FP 1
 w0 = 0
-s0 = 0
+s0 = 1
 P0 = 1
 y0 = [w0, s0, P0] 
-y0 = y0+np.array([np.random.uniform(0,0.1), np.random.uniform(0,0.1),-np.random.uniform(0,0.1)])
+y0 = y0+np.array([np.random.uniform(0,0.1), -np.random.uniform(0,0.1),-np.random.uniform(0,0.1)])
 
 # function for ODE
 def fun(t,y):
@@ -83,7 +80,7 @@ def fun(t,y):
   return [dwdt, dsdt, dPdt] # return array for dw/dt ds/dt
 
 
-tspan = [0,1000]
+tspan = [0,1500]
 
 fig, axs = plt.subplots(len(Ms), len(sais), layout="constrained")
 for i in range(len(Ms)):
@@ -94,11 +91,13 @@ for i in range(len(Ms)):
         [w,s,P] = sol.y
         t1 = sol.t
         axs[i, j].plot(t1,w,t1,s,'-.',t1,P,'--')
+        axs[i,j].set_ylim(-0.05,1.05)
         if i < len(Ms)-1:
             axs[i, j].set_xticklabels([])
-        axs[i, j].set_title(f"M = {M}, $s_{{ai}}$ = {sai}, $s_{{P}}$ = {sp}", fontsize=12)
+        axs[i, j].set_title(f"M = {M}, $s_{{ai}}$ = {sai}, $s_{{P}}$ = {sp}", fontsize=9)
         if j == len(sais)-1:
             plt.legend(["$w_D$","$s_{ID}$","$P$"])
+        
 
 
 
@@ -123,6 +122,6 @@ plt.show()
 
 
 
-fig.savefig("FP2_Nondim_Stability.pdf")
-fig.savefig("FP2_Nondim_Stability.eps",format='eps')
-fig.savefig("FP2_Nondim_Stability.png")
+fig.savefig("FP4_Nondim_Stability.pdf")
+fig.savefig("FP4_Nondim_Stability.eps",format='eps')
+fig.savefig("FP4_Nondim_Stability.png")

code/AI-Skill-FP-5-Testing-Nondim.py
@@ -43,12 +43,11 @@ from scipy.integrate import solve_ivp
 # amount of work needed to improve skill
 ws = 1/2
 
-# time scale for w
-a = 0.01
+
 # time scale for s
-b = 0.01
+b = 1
 # time scale for P 
-g = 0.01
+g = 1
 
 
 
@@ -63,7 +62,7 @@ sp = 1/2
 
 
 # Motivation Array
-Ms = np.array([-0.1,0.1])
+Ms = np.array([-0.25,0.25])
 #sais = np.array([sp/2,3*sp/2])
 wss = [1/2, 3/2]
 
@@ -90,9 +89,9 @@ def fun(t,y):
   return [dwdt, dsdt, dPdt] # return array for dw/dt ds/dt
 
 
-tspan = [0,10000]
+tspan = [0,1000]
 
-fig, axs = plt.subplots(len(Ms), len(wss))
+fig, axs = plt.subplots(len(Ms), len(wss), layout="constrained")
 for i in range(len(Ms)):
     M =Ms[i]
     for j in range(len(wss)):
@@ -100,11 +99,12 @@ for i in range(len(Ms)):
         sol = solve_ivp(fun, tspan, y0, method = 'RK45',atol=1e-10,rtol=1e-8) 
         [w,s,P] = sol.y
         t1 = sol.t
-        axs[i, j].plot(t1,w,t1,s,t1,P)
+        axs[i, j].plot(t1,w,t1,s,'-.',t1,P,'--')
         if i == 0:
             axs[i, j].set_xticklabels([])
-        axs[i, j].set_title(f"M = {M}, $w_{{s}}$ = {ws}")
-    
+        axs[i, j].set_title(f"M = {M}, $w_{{s}}$ = {ws}", fontsize=12)
+        if j == len(wss)-1:
+            plt.legend(["$w_D$","$s_{ID}$","$P$"])       
 
 
 
@@ -127,3 +127,6 @@ plt.show()
 #plt.show()
 
 
+fig.savefig("FP5_Nondim_Stability.pdf")
+fig.savefig("FP5_Nondim_Stability.eps",format='eps')
+fig.savefig("FP5_Nondim_Stability.png")

code/AI-Skill-FP-8-Testing-Nondim.py
@@ -32,29 +32,10 @@ from scipy.integrate import solve_ivp
 
 
 
-
-
-# amount of work needed to improve skill
-ws = 2
-# make skill (s^*)
-smax = 4
-# work hours in day
-wd = 6
-# Max amount of productivity
-Pmax = 10
-
-# time scale for w
-a = 0.01
 # time scale for s
-b = 0.01
+b = 1
 # time scale for P 
-g = 0.01
-
-
-
-#sai = 1.3*smax
-
-
+g = 1
 
 
 
@@ -88,11 +69,11 @@ def fun(t,y):
 
 
 
-tspan = [0,4000]
+tspan = [0,1000]
 
 
 sp = sps[0]
-fig, axs = plt.subplots(len(Ms), len(wss))
+fig, axs = plt.subplots(len(Ms), len(wss), layout="constrained")
 for i in range(len(Ms)):
     M =Ms[i]
     for j in range(len(wss)):
@@ -100,8 +81,12 @@ for i in range(len(Ms)):
         sol = solve_ivp(fun, tspan, y0, method = 'RK45',atol=1e-10,rtol=1e-8) 
         [w,s,P] = sol.y
         t1 = sol.t
-        axs[i, j].plot(t1,w,t1,s,t1,P)
-        axs[i, j].set_ylim([-0.1,1.1])
+        if i == 0:
+            axs[i, j].set_xticklabels([])
+        axs[i, j].set_title(f"M = {M}, $w_{{s}}$ = {ws}, $s_{{P}}$ = {sp}", fontsize=12)
+        axs[i, j].plot(t1,w,t1,s,'-.',t1,P,'--')
+       
+
 
 
 
@@ -110,7 +95,7 @@ plt.show()
 
 
 sp = sps[1]
-fig2, axs2 = plt.subplots(len(Ms), len(wss))
+fig2, axs2 = plt.subplots(len(Ms), len(wss), layout="constrained")
 for i in range(len(Ms)):
     M =Ms[i]
     for j in range(len(wss)):
@@ -118,9 +103,12 @@ for i in range(len(Ms)):
         sol = solve_ivp(fun, tspan, y0, method = 'RK45',atol=1e-10,rtol=1e-8) 
         [w,s,P] = sol.y
         t1 = sol.t
-        axs2[i, j].plot(t1,w,t1,s,t1,P)
-
-
+        if i == 0:
+            axs2[i, j].set_xticklabels([])
+        axs2[i, j].set_title(f"M = {M}, $w_{{s}}$ = {ws}, $s_{{P}}$ = {sp}", fontsize=12)
+        axs2[i, j].plot(t1,w,t1,s,'-.',t1,P,'--')
+        if j == len(wss)-1:
+            plt.legend(["$w_D$","$s_{ID}$","$P$"]) 
 
 
 #plt.ylabel("Function Values")
@@ -142,4 +130,12 @@ plt.show()
 
 #plt.show()
 
+fig.savefig("FP8_Nondim_Stability_sp_less_smax.pdf")
+fig.savefig("FP8_Nondim_Stability_sp_less_smax.eps",format='eps')
+fig.savefig("FP8_Nondim_Stability_sp_less_smax.png")
+
+
+fig2.savefig("FP8_Nondim_Stability_sp_greater_smax.pdf")
+fig2.savefig("FP8_Nondim_Stability_sp_greater_smax.eps",format='eps')
+fig2.savefig("FP8_Nondim_Stability_sp_greater_smax.png")
 

code/AI-Skill-FP-9-Testing-Nondim.py
@@ -57,11 +57,11 @@ g = 1
 
 # Motivation Array
 #Ms = np.array([Pmax/2, Pmax/2])
-M = 1/2
+#M = 1/2
 #sais = np.array([1.5*smax,1.5*smax])
 
-# sai, sp
-param1 = np.array([[0.5, 0.75, 0.75],[0.5, 0.87,0.75]])
+# sai, sp, ws,M
+param1 = np.array([[0.5, 0.25, 0.75,0.5],[0.5, 0.5,0.75,0],[0.5, 0.1,0.1,0]])
 
 #M =1
 
@@ -79,44 +79,46 @@ def fun(t,y):
   return [dwdt, dsdt, dPdt] # return array for dw/dt ds/dt
 
 
-tspan = [0,2500]
+tspan = [0,10000]
 
 
 paramshape= np.shape(param1)
 
 
 
-fig, axs = plt.subplots(1,2, layout="constrained")
+
+fig, axs = plt.subplots(1,3, layout="constrained")
 for i in range(paramshape[0]):
-    sai,sp,ws= param1[i]
-    w0 = ws
-    s0 = (sp-(1-ws)*sai)/ws
+    sai,sp,ws,M= param1[i]
+    w0 = 1-sp/sai
+    s0 = 0
     P0 = M
     y0 = [w0, s0, P0]
     print(y0)
     
+    perturb = np.array([np.random.uniform(-0.01,0.01), np.random.uniform(0,0.01),np.random.uniform(-0.01,0.01)])
+  
 
-    if s0 ==0:
-        y0 = y0 +1*np.array([np.random.uniform(-0.01,0.01), np.random.uniform(0,0.01),np.random.uniform(-0.01,0.01)])
-    elif s0 ==1.0:
-        y0 = y0 +1*np.array([np.random.uniform(-0.01,0.01), np.random.uniform(-0.01,0),np.random.uniform(-0.01,0.01)])
-    else:
-        y0 = y0 +1*np.array([np.random.uniform(-0.1,0.1), np.random.uniform(-0.1,0),np.random.uniform(-0.1,0.1)])
+    if sp ==sai:
+        perturb[0] = np.random.uniform(0,0.01)
+    if M == 1:
+        perturb[2] = np.random.uniform(-0.01,0)
+    if M == 0:
+        perturb[2] = np.random.uniform(0,0.01)
+        
+    y0 = y0 +perturb
     sol = solve_ivp(fun, tspan, y0, method = 'RK45',atol=1e-10,rtol=1e-8) 
     [w,s,P] = sol.y
     t1 = sol.t
     #print(int(np.mod(i/2,2)))
     axs[i].plot(t1,w,t1,s,'-.',t1,P,'--')
-    axs[i].set_ylim([-0.1,1.1])
-    axs[i].set_title(f"$s_{{ai}}$ = {sai}, $s_{{P}}$ = {sp}, $w_{{s}}$ = {ws}", fontsize=12)
+    #axs[i].set_ylim([-0.1,1.1])
+    axs[i].set_title(f"$s_{{ai}}$ = {sai}, $s_{{P}}$ = {sp}, $w_{{s}}$ = {ws}, $M$ = {M}",fontsize=7)
     if i == paramshape[0]-1:
         plt.legend(["$w_D$","$s_{ID}$","$P$"])
 
 
 
-fig.savefig("FP11_Nondim_Stability.pdf")
-fig.savefig("FP11_Nondim_Stability.eps",format='eps')
-fig.savefig("FP11_Nondim_Stability.png")
 
 
 